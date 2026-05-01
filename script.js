0ddb8ea78a59446c167abdc399526d52ee709966
bimkagg2 <bimkagg2@gmail.com>
2026-05-01 16:12:16 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function openTip(card) {
-  document.querySelectorAll('.tip-card').forEach(el => {
-    if (el !== card) el.classList.remove('active');
-  });
-
-  card.classList.toggle('active');
-}
